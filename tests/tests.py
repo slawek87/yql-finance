7c7c9684c184921ad1ea2aa785473e697a9d840d
Sławomir Kabik <slawek@redsoftware.pl>
2014-12-24 15:43:25 +0100

    update

---
+++ tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+from api.main import YQL
+
+
+class YQLTest(unittest.TestCase):
+    def setUp(self):
+        self.yql = YQL()
+
+    def test_appl(self):
+        yql = YQL('AAPL', '2014-01-10', '2014-01-10')
+
+        self.assertEqual('2014-01-10', yql.get_prices()[0].get('date'))
+        self.assertEqual('74.57', yql.get_prices()[0].get('price'))
+
+        self.yql.select('AAPL', '2014-01-10', '2014-01-10')
+
+        self.assertEqual('2014-01-10', self.yql.get_prices()[0].get('date'))
+        self.assertEqual('74.57', self.yql.get_prices()[0].get('price'))
