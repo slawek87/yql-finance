c14c3a81fb9b231bc0eea7cdc31202b6196df29b
Sławomir Kabik <slawek@redsoftware.pl>
2014-12-26 11:50:03 +0100

    cleaning

--- api/main.py
+++ api/main.py
@@ -1,38 +1,8 @@
 import datetime
 from dateutil.relativedelta import relativedelta
-import requests
 
-ONE_YEAR = 1
-
-
-class Request(object):
-    parameters = {
-        'q': '',
-        'format': 'json',
-        'diagnostics': 'false',
-        'env': 'store://datatables.org/alltableswithkeys',
-        'callback': ''
-    }
-    api = 'https://query.yahooapis.com/v1/public/yql'
-
-    def prepare_query(self, symbol,  start_date, end_date):
-        """Method returns prepared request query for Yahoo YQL API."""
-        query = \
-            'select * from yahoo.finance.historicaldata where symbol = "%s" and startDate = "%s" and endDate = "%s"' \
-            % (symbol, start_date, end_date)
-
-        return query
-
-    def send(self, symbol, start_date, end_date):
-        """Method sends request to Yahoo YQL API."""
-        query = self.prepare_query(symbol, start_date, end_date)
-
-        self.parameters['q'] = query
-        response = requests.get(self.api, params=self.parameters).json()
-
-        results = response['query']['results']['quote']
-
-        return results
+from api.request import Request
+from api import const
 
 
 class YQL(object):
@@ -85,7 +55,7 @@ class YQL(object):
 
     def fetch_data(self):
         """Method returns results from response of Yahoo YQL API. It should returns always python list."""
-        if relativedelta(self.end_date, self.start_date).years <= ONE_YEAR:
+        if relativedelta(self.end_date, self.start_date).years <= const.ONE_YEAR:
             data = self.request.send(self.symbol, self.start_date, self.end_date)
         else:
             data = self.fetch_chunk_data()
---
+++ api/request.py
@@ -0,0 +1,31 @@
+import requests
+
+
+class Request(object):
+    parameters = {
+        'q': '',
+        'format': 'json',
+        'diagnostics': 'false',
+        'env': 'store://datatables.org/alltableswithkeys',
+        'callback': ''
+    }
+    api = 'https://query.yahooapis.com/v1/public/yql'
+
+    def prepare_query(self, symbol,  start_date, end_date):
+        """Method returns prepared request query for Yahoo YQL API."""
+        query = \
+            'select * from yahoo.finance.historicaldata where symbol = "%s" and startDate = "%s" and endDate = "%s"' \
+            % (symbol, start_date, end_date)
+
+        return query
+
+    def send(self, symbol, start_date, end_date):
+        """Method sends request to Yahoo YQL API."""
+        query = self.prepare_query(symbol, start_date, end_date)
+
+        self.parameters['q'] = query
+        response = requests.get(self.api, params=self.parameters).json()
+
+        results = response['query']['results']['quote']
+
+        return results
