52b1ac6720db09c4d8a9864b171506e90a8d3964
Sławomir Kabik <slawek@redsoftware.pl>
2015-08-29 13:41:06 +0200

    fixed tests

--- tests/tests.py
+++ tests/tests.py
@@ -7,9 +7,9 @@ class YQLTest(unittest.TestCase):
         yql_data = YQL.select('AAPL', '2014-01-10', '2014-01-10')
 
         self.assertEqual(YQL.to_date('2014-01-10'), yql_data[0].get('date'))
-        self.assertEqual('74.57', yql_data[0].get('price'))
+        self.assertEqual('73.629744', yql_data[0].get('price'))
 
         yql_data = YQL('AAPL', '2014-01-10', '2014-01-10')
 
         self.assertEqual(YQL.to_date('2014-01-10'), yql_data[0].get('date'))
-        self.assertEqual('74.57', yql_data[0].get('price'))
+        self.assertEqual('73.629744', yql_data[0].get('price'))
